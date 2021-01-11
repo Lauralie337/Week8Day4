55ccea3c0c3f7f8525b866772cea43781dbffc6c
Lauralie337 <72320233+Lauralie337@users.noreply.github.com>
2021-01-10 18:32:02 -0600

    First push
    initial app

---
+++ src/com/tts/Main.java
@@ -0,0 +1,42 @@
+package com.tts;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        // illegal fields
+
+//        int int = 1; //java will not let you name a class after some types
+//        byte byte = 0;
+//        String String = "String"; //but sometimes will
+//        int class = 4; //java also will not let you call things class
+
+//        System.out.println(int);
+//        java: not a statement
+
+//        System.out.println(byte);
+//        java: not a statement
+
+//        System.out.println(String);
+        //this is fine
+
+//        System.out.println(class);
+//        java: not a statement
+
+
+
+        //Uninitialized variables
+
+//        int zero;
+//        System.out.println(zero);
+
+//        String tryIt;
+//        System.out.println(tryIt);
+//        java: variable tryIt might not have been initialized
+
+
+
+    }
+}
